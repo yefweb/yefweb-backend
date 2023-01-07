f83ddabdbe443d0a88ec14b66ed3182ae583534b
yefweb <yeferson.dev@gmail.com>
2023-01-06 21:52:02 -0500

    schemas delete

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,19 +1,5 @@
-import {testimonials} from './testimonials'
 import {works} from './works'
 import {abouts} from './abouts'
-import {brands} from './brands'
-import {contact} from './contact'
-import {experiences} from './experiences'
 import {skills} from './skills'
-import {workExperience} from './workExperience'
 
-export const schemaTypes = [
-  testimonials,
-  works,
-  abouts,
-  brands,
-  contact,
-  experiences,
-  skills,
-  workExperience,
-]
+export const schemaTypes = [works, abouts, skills]
